4249ae55cb30c7729d9809d274ced169b47b1f66
Luis Angel Ponce Alvarez <poncealvarezluisangel4@gmail.com>
2023-06-08 20:09:00 -0600

    añado titulo de la ventana y creo la ventana

--- GestorDeNegocios.py
+++ GestorDeNegocios.py
@@ -1,8 +1,11 @@
 from tkinter import *
 
+# Crear una ventana 
 ventana = Tk()
 ventana.title("Gestor De Negocios")
 ventana.geometry("600x600")
+
+# Creando titulo 
 
 lbl = Label(ventana, text="Gestiona tus productos aquí")
 lbl.pack()
